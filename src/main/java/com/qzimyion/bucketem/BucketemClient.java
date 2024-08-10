a32dc50221c34546e717ebd4f1f952af2ffcb7d3
Qzimyion <samyakchauhan676@gmail.com>
2024-08-11 01:59:20 +0530

    Added an ModelPredicate for Turtle buckets

--- src/main/java/com/qzimyion/bucketem/BucketemClient.java
+++ src/main/java/com/qzimyion/bucketem/BucketemClient.java
@@ -3,22 +3,31 @@ package com.qzimyion.bucketem;
 import com.qzimyion.bucketem.items.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
 import net.minecraft.util.Identifier;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @SuppressWarnings("deprecation")
 public class BucketemClient implements ClientModInitializer {
 
-//    public static void modelPredicates(){
-//        FabricModelPredicateProviderRegistry.register(ModItems.TURTLE_BUCKET, Identifier.ofVanilla("age"), (itemStack, clientWorld, livingEntity, i) -> {
-//            float age = 1;
-//            if (itemStack.getComponents() != null && itemStack.getTag().contains("Age") && itemStack.getTag().getInt("Age") < 0)
-//                age = 0;
-//            return age;
-//        });
-//    }
+    public static void modelPredicates(){
+        FabricModelPredicateProviderRegistry.register(ModItems.TURTLE_BUCKET, Identifier.ofVanilla("age"), (itemStack, clientWorld, livingEntity, i) -> {
+            AtomicBoolean ageBL = new AtomicBoolean(false);
+            NbtComponent.set(DataComponentTypes.BUCKET_ENTITY_DATA, itemStack, nbt -> {
+                nbt.get("Age");
+                ageBL.set(nbt.getInt("Age") < 0);
+            });
+            float age = 1;
+            if (itemStack.getComponents() != null && itemStack.contains(DataComponentTypes.BUCKET_ENTITY_DATA) && ageBL.get())
+                age = 0;
+            return age;
+        });
+    }
 
 
     public void onInitializeClient(){
-//        modelPredicates();
+        modelPredicates();
     }
 }
