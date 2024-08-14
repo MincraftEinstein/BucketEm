5766b2828e8520ca077a99c1dd9d20f84316f4b1
Qzimyion <samyakchauhan676@gmail.com>
2024-08-14 17:51:41 +0530

    Backported 1.21 SlimeBottle features

--- src/main/java/com/qzimyion/bucketem/items/NewItems/Bottles/SlimeBottle.java
+++ src/main/java/com/qzimyion/bucketem/items/NewItems/Bottles/SlimeBottle.java
@@ -1,9 +1,11 @@
 package com.qzimyion.bucketem.items.NewItems.Bottles;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
@@ -13,13 +15,18 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.ChunkRandom;
 import net.minecraft.world.World;
 
 import java.util.Objects;
 
 public class SlimeBottle extends Item {
+    protected boolean enableSlimeChunkExcitement = true;
 
     public SlimeBottle(Item.Settings settings) {
         super(settings);
@@ -57,5 +64,35 @@ public class SlimeBottle extends Item {
             entity.setSize(size, false);
         }
         return ActionResult.CONSUME;
+    }
+
+    @Override
+    public boolean allowNbtUpdateAnimation(PlayerEntity player, Hand hand, ItemStack oldStack, ItemStack newStack) {
+        return false;
+    }
+
+    public static boolean isSlimeChunk(ServerWorld world, int x, int z) {
+        ChunkPos chunkpos = new ChunkPos(new BlockPos(x, 0, z));
+        return ChunkRandom.getSlimeRandom(chunkpos.x, chunkpos.z, world.getSeed(), 987234911L).nextInt(10) == 0;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if(world instanceof ServerWorld serverWorld) {
+            boolean chunkFinder = Boolean.TRUE.equals(stack.getOrCreateNbt().getBoolean("SlimeChunk"));
+            Vec3d pos = entity.getPos();
+            int x = (int) Math.floor(pos.x);
+            int z = (int) Math.floor(pos.z);
+            if (isSlimeChunk(serverWorld, x, z)){
+                if (enableSlimeChunkExcitement){
+                    if (chunkFinder != isSlimeChunk(serverWorld, x, z)){
+                        stack.getOrCreateNbt().putBoolean("SlimeChunk", isSlimeChunk(serverWorld, x, z));
+                    }
+                }
+            }
+            if (!isSlimeChunk(serverWorld, x, z)){
+                stack.getOrCreateNbt().putBoolean("SlimeChunk", false);
+            }
+        }
     }
 }
