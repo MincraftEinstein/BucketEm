941555ae9fc4ec6aef0925aaca1fede0d3518c75
Qzimyion <samyakchauhan676@gmail.com>
2024-09-22 02:53:28 +0530

    Added entity tags

---
+++ src/main/java/com/qzimyion/bucketem/ModTags.java
@@ -0,0 +1,17 @@
+package com.qzimyion.bucketem;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+
+    public static class EntityTypeTags{
+        public static final TagKey<EntityType<?>> MILKABLE_ENTITY = createTag("milkable_entity");
+
+        private static TagKey<EntityType<?>> createTag(String name) {
+            return TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(Bucketem.MOD_ID, name));
+        }
+    }
+}
