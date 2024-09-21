8995a10cfa8d0e06dc7ee11724d0a9e3f39473f9
Qzimyion <samyakchauhan676@gmail.com>
2024-09-22 02:56:18 +0530

    Updated english language datagen stuff

--- src/main/java/com/qzimyion/bucketem/datagen/langDatagen/BritishEnglishDatagen.java
+++ src/main/java/com/qzimyion/bucketem/datagen/langDatagen/BritishEnglishDatagen.java
@@ -13,6 +13,7 @@ public class BritishEnglishDatagen extends FabricLanguageProvider {
 
     @Override
     public void generateTranslations(TranslationBuilder translationBuilder) {
+        //Buckets
         translationBuilder.add(STRIDER_BUCKET, "Bucket of Strider");
         translationBuilder.add(SQUID_BUCKET, "Bucket of Squid");
         translationBuilder.add(GLOW_SQUID_BUCKET, "Bucket of Glow Squid");
@@ -23,13 +24,21 @@ public class BritishEnglishDatagen extends FabricLanguageProvider {
         translationBuilder.add(DRY_TROPICAL_FROG_BUCKET, "Dry Bucket of Tropical Frog");
         translationBuilder.add(DRY_TUNDRA_FROG_BUCKET, "Dry Bucket of Tundra Frog");
         translationBuilder.add(TURTLE_BUCKET, "Bucket of Turtle");
+        //Books
         translationBuilder.add(ALLAY_POSSESSED_BOOK, "Allay Possessed Book");
         translationBuilder.add(VEX_POSSESSED_BOOK, "Vex Possessed Book");
+        //Bottle
         translationBuilder.add(BEE_BOTTLE, "Bee in a Bottle");
         translationBuilder.add(SILVERFISH_BOTTLE, "Silverfish in a Bottle");
         translationBuilder.add(ENDERMITE_BOTTLE, "Endermite in a Bottle");
         translationBuilder.add(SLIME_BOTTLE, "Slime in a Bottle");
         translationBuilder.add(MAGMA_CUBE_BOTTLE, "Magma Cube in a Bottle");
+        translationBuilder.add(GOLDEN_BUCKET, "Golden Bucket");
+        translationBuilder.add(GOLDEN_WATER_BUCKET, "Golden Water Bucket");
+        translationBuilder.add(GOLDEN_LAVA_BUCKET, "Golden Lava Bucket");
+        translationBuilder.add(GOLDEN_POWDER_SNOW_BUCKET, "Golden Powder Snow Bucket");
+        translationBuilder.add(GOLDEN_MILK_BUCKET, "Golden Milk Bucket");
+        //Status effect
         translationBuilder.add(ModStatusEffectsRegistry.BLISTERED_VISION, "Blistered Vision");
     }
 }
