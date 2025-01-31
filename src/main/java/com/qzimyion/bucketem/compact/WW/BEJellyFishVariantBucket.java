52eeb9e43d3b4a53321ed0e8c0585798e0822176
Qzimyion <samyakchauhan676@gmail.com>
2025-01-31 16:48:47 +0530

    Updated datagen stuff

--- src/main/java/com/qzimyion/bucketem/compact/WW/BEJellyFishVariantBucket.java
+++ src/main/java/com/qzimyion/bucketem/compact/WW/BEJellyFishVariantBucket.java
@@ -1,5 +1,8 @@
-package com.qzimyion.bucketem.items.NewItems.FrogBuckets;
+package com.qzimyion.bucketem.compact.WW;
 
+import net.frozenblock.wilderwild.entity.Jellyfish;
+import net.frozenblock.wilderwild.entity.variant.JellyfishVariant;
+import net.frozenblock.wilderwild.registry.WWEntityTypes;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.FrogEntity;
@@ -7,17 +10,23 @@ import net.minecraft.entity.passive.FrogVariant;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class TropicalFrogBuckets extends BucketItem {
+public class BEJellyFishVariantBucket extends BucketItem {
 
-    public TropicalFrogBuckets(Fluid fluid, Settings settings) {
+    final JellyfishVariant variant;
+    public BEJellyFishVariantBucket(Fluid fluid, JellyfishVariant variant, Item.Settings settings) {
         super(fluid, settings);
+        this.variant = variant;
     }
 
     @Override
@@ -28,11 +37,16 @@ public class TropicalFrogBuckets extends BucketItem {
         }
     }
 
+    @Override
+    public void playEmptyingSound(@Nullable PlayerEntity player, WorldAccess world, BlockPos pos) {
+        world.playSound(player, pos, SoundEvents.ITEM_BUCKET_EMPTY_FISH, SoundCategory.NEUTRAL, 1.0F, 1.0F);
+    }
+
     public void spawnEntity(ServerWorld world, ItemStack stack, BlockPos pos) {
-        FrogEntity entity = EntityType.FROG.spawnFromItemStack(world, stack, null, pos, SpawnReason.BUCKET, true, false);
+        Jellyfish entity = WWEntityTypes.JELLYFISH.spawnFromItemStack(world, stack, null, pos, SpawnReason.BUCKET, true, false);
         if (entity != null) {
             entity.setPersistent();
-            entity.setVariant(FrogVariant.WARM);
+            entity.setVariant(this.variant);
         }
     }
 }
--- src/main/java/com/qzimyion/bucketem/datagen/langDatagen/BritishEnglishDatagen.java
+++ src/main/java/com/qzimyion/bucketem/datagen/langDatagen/BritishEnglishDatagen.java
@@ -39,6 +39,6 @@ public class BritishEnglishDatagen extends FabricLanguageProvider {
         translationBuilder.add(GOLDEN_POWDER_SNOW_BUCKET, "Golden Powder Snow Bucket");
         translationBuilder.add(GOLDEN_MILK_BUCKET, "Golden Milk Bucket");
         //Status effect
-        translationBuilder.add(ModStatusEffectsRegistry.BLISTERED_VISION, "Blistered Vision");
+        translationBuilder.add(ModStatusEffectsRegistry.MAGMA_VISION, "Blistered Vision");
     }
 }
