7ae91ca4409d58f81f51c86440d475af2f5225ca
Qzimyion <samyakchauhan676@gmail.com>
2025-01-31 17:07:43 +0530

    Removed item tags stuff for stew as the feature was scrapped

--- src/main/java/com/qzimyion/bucketem/ModTags.java
+++ src/main/java/com/qzimyion/bucketem/ModTags.java
@@ -6,8 +6,8 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModTags {
-
-    public static class EntityTypeTags{
+    public static class EntityTypeTagsForMod {
+        
         public static final TagKey<EntityType<?>> MILKABLE_ENTITY = createTag("milkable_entity");
 
         private static TagKey<EntityType<?>> createTag(String name) {
