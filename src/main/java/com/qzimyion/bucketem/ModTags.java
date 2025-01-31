e4857b3be31a0d8098edbe4a646db5508b6028f0
Qzimyion <samyakchauhan676@gmail.com>
2025-01-31 17:09:46 +0530

    idfk

--- src/main/java/com/qzimyion/bucketem/ModTags.java
+++ src/main/java/com/qzimyion/bucketem/ModTags.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 
 public class ModTags {
     public static class EntityTypeTagsForMod {
-        
+
         public static final TagKey<EntityType<?>> MILKABLE_ENTITY = createTag("milkable_entity");
 
         private static TagKey<EntityType<?>> createTag(String name) {
