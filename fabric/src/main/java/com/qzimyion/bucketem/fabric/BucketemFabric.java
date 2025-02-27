e49bc034dda55b56eaad0a73db237c33f2a94e79
Qzimyion <samyakchauhan676@gmail.com>
2025-02-27 12:56:21 +0530

    :3

--- fabric/src/main/java/com/qzimyion/bucketem/fabric/BucketemFabric.java
+++ fabric/src/main/java/com/qzimyion/bucketem/fabric/BucketemFabric.java
@@ -1,19 +1,13 @@
 package com.qzimyion.bucketem.fabric;
 
 import com.qzimyion.bucketem.core.registry.DispenserBehaviorRegistry;
-import com.qzimyion.bucketem.core.registry.ModCreativeTabs;
 import com.qzimyion.bucketem.core.registry.ModEvents;
 import com.qzimyion.bucketem.core.registry.ModItems;
-import com.qzimyion.bucketem.platform.fabric.CommonHelperImpl;
 import dev.architectury.registry.fuel.FuelRegistry;
 import net.fabricmc.api.ModInitializer;
 
 import com.qzimyion.bucketem.BucketEmCommon;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Items;
 
 public final class BucketemFabric implements ModInitializer {
     @Override
