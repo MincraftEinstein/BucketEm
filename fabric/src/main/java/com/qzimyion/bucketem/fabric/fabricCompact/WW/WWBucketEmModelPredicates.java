796e68cc519a5fcb42fd2840ede2f05321371c14
Qzimyion <samyakchauhan676@gmail.com>
2025-03-25 02:51:13 +0530

    Temporarily removed compatibility with WW

--- fabric/src/main/java/com/qzimyion/bucketem/fabric/fabricCompact/WW/WWBucketEmModelPredicates.java
+++ fabric/src/main/java/com/qzimyion/bucketem/fabric/fabricCompact/WW/WWBucketEmModelPredicates.java
@@ -1,49 +1,57 @@
 package com.qzimyion.bucketem.fabric.fabricCompact.WW;
 
-import com.qzimyion.bucketem.fabric.mixin.EntityBucketItemAccessor;
-import com.qzimyion.bucketem.platform.ClientHelper;
-import net.frozenblock.wilderwild.entity.Jellyfish;
-import net.frozenblock.wilderwild.entity.variant.JellyfishVariant;
-import net.frozenblock.wilderwild.registry.WWItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.animal.Bucketable;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.MobBucketItem;
+//import com.qzimyion.bucketem.core.mixin.ItemMixins.EntityBucketItemAccessor;
+//import com.qzimyion.bucketem.platform.ClientHelper;
+//import net.frozenblock.wilderwild.entity.Jellyfish;
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.client.multiplayer.ClientLevel;
+//import net.minecraft.core.component.DataComponents;
+//import net.minecraft.world.entity.Entity;
+//import net.minecraft.world.entity.LivingEntity;
+//import net.minecraft.world.item.Item;
+//import net.minecraft.world.item.ItemStack;
+//import net.minecraft.world.item.MobBucketItem;
+//import net.minecraft.world.item.component.CustomData;
 
 public class WWBucketEmModelPredicates {
 
-    public static void register(ClientHelper.ModelPredicates event){
+//    private static Jellyfish getJellyfishlRef(ItemStack itemStack, ClientLevel level, LivingEntity holder, int seed) {
+//        Item item = itemStack.getItem();
+//        if (!(item instanceof MobBucketItem)) {
+//            return null;
+//        }
+//        assert Minecraft.getInstance().level != null;
+//        Entity entity = ((EntityBucketItemAccessor) item).type().create(Minecraft.getInstance().level);
+//        if (!(entity instanceof Jellyfish jellyfish)) {
+//            return null;
+//        }
+//        CustomData data = itemStack.getOrDefault(DataComponents.BUCKET_ENTITY_DATA, CustomData.EMPTY);
+//        jellyfish.loadFromBucketTag(data.copyTag());
+//        return jellyfish;
+//    }
+
+//    public static void register(ClientHelper.ModelPredicates event){
         //==Wilder wild==//
         //Jellyfish variants
-        event.register(WWItems.JELLYFISH_BUCKET, new ResourceLocation("variant"), (itemStack, world, holder, seed) -> {
-            Item item = itemStack.getItem();
-            if (!(item instanceof MobBucketItem)) {
-                return 0f;
-            }
-            assert Minecraft.getInstance().level != null;
-            Entity entity = ((EntityBucketItemAccessor) item).type().create(Minecraft.getInstance().level);
-            if (!(entity instanceof Jellyfish)) {
-                return 0f;
-            }
-            ((Bucketable) entity).loadFromBucketTag(itemStack.getOrCreateTag());
-            ResourceLocation texture = ((Jellyfish) entity).getVariant().texture();
-            return texture == JellyfishVariant.PINK.texture() ? 0 : (float) (texture == JellyfishVariant.BLUE.texture() ? 0.1 : texture == JellyfishVariant.LIME.texture() ? 0.2 : texture == JellyfishVariant.RED.texture() ? 0.3 : texture == JellyfishVariant.YELLOW.texture() ? 0.4 : texture == JellyfishVariant.PEARLESCENT_BLUE.texture() ? 0.5 : 0.6);
-        });
-        //Jellyfish age
-        event.register(WWItems.JELLYFISH_BUCKET, new ResourceLocation("age"), (itemStack, clientLevel, livingEntity, i) -> {
-            Item item = itemStack.getItem();
-            if (!(item instanceof MobBucketItem)) {
-                return 0f;
-            }
-            assert Minecraft.getInstance().level != null;
-            Entity entity = ((EntityBucketItemAccessor) item).type().create(Minecraft.getInstance().level);
-            if (!(entity instanceof Jellyfish)) {
-                return 0f;
-            }
-            ((Bucketable) entity).loadFromBucketTag(itemStack.getOrCreateTag());
-            return ((Jellyfish) entity).getAge() > 0 ? 1 : 0;
-        });
-    }
+//        event.register(WWItems.JELLYFISH_BUCKET, ResourceLocation.parse("variant"), (itemStack, world, holder, seed) -> {
+//            Jellyfish jellyfish = getJellyfishlRef(itemStack, world, holder, seed);
+//            if (jellyfish == null) {
+//                return 0f;
+//            }
+//            ResourceKey<JellyfishVariant> texture = jellyfish.;
+//            return texture == JellyfishVariants.PINK ? 0f : 1f;
+//        });
+//        //Jellyfish age
+//        event.register(WWItems.JELLYFISH_BUCKET, ResourceLocation.parse("age"), (itemStack, clientLevel, livingEntity, i) -> {
+//            AtomicBoolean ageBL = new AtomicBoolean(false);
+//            CustomData.update(DataComponents.BUCKET_ENTITY_DATA, itemStack, nbt -> {
+//                nbt.get("age");
+//                ageBL.set(nbt.getInt("age") < 0);
+//            });
+//            float age = 1;
+//            if (itemStack.has(DataComponents.BUCKET_ENTITY_DATA) && ageBL.get())
+//                age = 0;
+//            return age;
+//        });
+//    }
 }
