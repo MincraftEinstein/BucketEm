bfc17ede52d63affd1388cb0cf83b38eaf1e710b
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-06-11 13:48:47 -0400

    Rewrote SolidBucketItemMixin

--- common/src/main/java/com/qzimyion/bucketem/core/mixin/ItemMixins/SolidBucketItemMixin.java
+++ common/src/main/java/com/qzimyion/bucketem/core/mixin/ItemMixins/SolidBucketItemMixin.java
@@ -1,37 +1,26 @@
 package com.qzimyion.bucketem.core.mixin.ItemMixins;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.Block;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.SolidBucketItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(SolidBucketItem.class)
-public class SolidBucketItemMixin extends BlockItem {
+public class SolidBucketItemMixin {
 
-
-    public SolidBucketItemMixin(Block block, Properties properties) {
-        super(block, properties);
-    }
-
-    @Override
-    public @NotNull InteractionResult useOn(UseOnContext useOnContext) {
-        Player player = useOnContext.getPlayer();
-        InteractionHand hand = useOnContext.getHand();
-        InteractionResult interactionResult = super.useOn(useOnContext);
-        if (interactionResult.consumesAction() && player != null && !player.isCreative()) {
-            if (!player.getAbilities().instabuild) {
-                ItemStack emptyBucket = new ItemStack(Items.BUCKET);
-                if (!player.getInventory().add(emptyBucket)) {
-                    player.drop(emptyBucket, false);
-                }
-            } else {
-                player.setItemInHand(hand, new ItemStack(Items.BUCKET));
+    @WrapWithCondition(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setItemInHand(Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;)V"))
+    private boolean addOrDropBucket(Player player, InteractionHand hand, ItemStack stack) {
+        if (!player.getAbilities().instabuild) {
+            ItemStack emptyBucket = new ItemStack(Items.BUCKET);
+            if (!player.getInventory().add(emptyBucket)) {
+                player.drop(emptyBucket, false);
             }
+            return false;
         }
-        return interactionResult;
+        return true;
     }
 }
