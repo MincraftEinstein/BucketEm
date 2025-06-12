385306da979cfab6f5cdaf559b053946516cb77a
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-06-11 22:09:22 -0400

    More mixin changes

--- common/src/main/java/com/qzimyion/bucketem/core/mixin/entities/MultiEntityMixin.java
+++ common/src/main/java/com/qzimyion/bucketem/core/mixin/entities/MultiEntityMixin.java
@@ -2,19 +2,35 @@ package com.qzimyion.bucketem.core.mixin.entities;
 
 import com.qzimyion.bucketem.util.BuckEmBucketable;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.GlowSquid;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.entity.animal.Squid;
 import net.minecraft.world.entity.animal.Turtle;
-import net.minecraft.world.entity.monster.Silverfish;
-import net.minecraft.world.entity.monster.Strider;
+import net.minecraft.world.entity.animal.allay.Allay;
+import net.minecraft.world.entity.animal.frog.Frog;
+import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @SuppressWarnings("deprecation")
-@Mixin({Strider.class, GlowSquid.class, Squid.class, Silverfish.class, Turtle.class})
+@Mixin({
+        Strider.class,
+        GlowSquid.class,
+        Squid.class,
+        Turtle.class,
+        Frog.class,
+        Silverfish.class,
+        Endermite.class,
+        Slime.class,
+        MagmaCube.class,
+        Allay.class,
+        Vex.class,
+        Bee.class
+})
 public abstract class MultiEntityMixin implements BuckEmBucketable {
 
     @Unique
@@ -36,10 +52,31 @@ public abstract class MultiEntityMixin implements BuckEmBucketable {
     @Override
     public void saveToBucketTag(ItemStack stack) {
         Bucketable.saveDefaultDataToBucketTag(bucketem$me, stack);
+
+        if (bucketem$me instanceof Bee bee) {
+            CompoundTag tag = stack.getOrCreateTag();
+            tag.putBoolean("HasNectar", bee.hasNectar());
+            tag.putBoolean("HasStung", bee.hasStung());
+            tag.putInt("Age", bee.getAge());
+            tag.putFloat("Health", bee.getHealth());
+            bee.addPersistentAngerSaveData(tag);
+        }
     }
 
     @Override
     public void loadFromBucketTag(CompoundTag tag) {
         Bucketable.loadDefaultDataFromBucketTag(bucketem$me, tag);
+
+        if (bucketem$me instanceof Bee bee) {
+            bee.setHasNectar(tag.getBoolean("HasNectar"));
+            bee.setHasStung(tag.getBoolean("HasStung"));
+            bee.setAge(tag.getInt("Age"));
+            bee.setHealth(tag.getFloat("Health"));
+            bee.readPersistentAngerSaveData(bee.level(), tag);
+
+            if (tag.contains("Anger", Tag.TAG_INT)) {
+                bee.setRemainingPersistentAngerTime(tag.getInt("Anger"));
+            }
+        }
     }
 }
